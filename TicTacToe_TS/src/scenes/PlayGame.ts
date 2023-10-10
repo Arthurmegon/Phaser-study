dd395316099240808af5b27dae8ce51082745caa
arthur <arthur.gonzalez@empirica.digital>
2023-10-10 08:01:25 -0300

    style: Tic Tac Toe resize

--- TicTacToe_TS/src/scenes/PlayGame.ts
+++ TicTacToe_TS/src/scenes/PlayGame.ts
@@ -12,14 +12,17 @@ export default class PlayGame extends Phaser.Scene{
   }
 
   create() {
+    // TITLE
     this.add.text(this.game.config.width as number / 2.9, this.game.config.height as number / 5, 'JOGO DA VELHA',  {fontSize: '80px', color: '#ffff', fontFamily: 'monospace'});
 
+    // VETOR TABULEIRO
     this.tabuleiro = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' ']
     ];
 
+    // DISTRIBUIÇÃO LINHAS E COLUNAS
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
          const quadrado = this.add.sprite(820 + j * 120, 450 + i * 120, 'quadrado').setInteractive();
@@ -28,24 +31,23 @@ export default class PlayGame extends Phaser.Scene{
         quadrado.on('pointerdown', this.marcar, this);
       }
     }
-
-    this.add.text(100, 250, '', { fontSize: '32px', color: '#fff' }).setOrigin(0.5, 0);
   }
 
+  // MARCAR NO TABULEIRO
   marcar(pointer: Phaser.Input.Pointer) {
-    const jogadaText = this.add.text(100, 250, '', { fontSize: '32px', color: '#fff' });
-    const row = Math.floor((pointer.y - 450) / 120);
-    const col = Math.floor((pointer.x - 820) / 120);
+ 
+    const row = Math.floor((pointer.y - 425) / 115);
+    const col = Math.floor((pointer.x - 810) / 115);
 
     if (this.tabuleiro[row][col] !== 'X' && this.tabuleiro[row][col] !== 'O') {
       this.jogada++;
 
       if (this.xOry === 0) {
-        this.add.text(820 + col * 120, 450 + row * 120, 'X', { fontSize: '48px', color: '#f00' });
+        this.add.text(810 + col * 115, 425 + row * 115, 'X', { fontSize: '48px', color: '#f00', fontFamily: 'monospace' });
         this.xOry = 1;
         this.tabuleiro[row][col] = 'X';
       } else {
-        this.add.text(820 + col * 120, 450 + row * 120, 'O', { fontSize: '48px', color: '#00f' });
+        this.add.text(810 + col * 115, 425 + row * 115, 'O', { fontSize: '48px', color: '#00f', fontFamily: 'monospace'});
         this.xOry = 0;
         this.tabuleiro[row][col] = 'O';
       }
@@ -54,14 +56,19 @@ export default class PlayGame extends Phaser.Scene{
     this.comparar();
     if (this.resultado > 0) {
       if (this.resultado === 1) {
-        jogadaText.setText('Parabéns X, você ganhou');
+        this.add.text(1250, 350, 'Parabéns X, você ganhou!', { fontSize: '35px', color: '#fff', fontFamily: 'monospace'});
       } else {
-        jogadaText.setText('Parabéns O, você ganhou');
+        this.add.text(150, 350, 'Parabéns O, você ganhou!', { fontSize: '35px', color: '#fff', fontFamily: 'monospace'});
       }
     } else if (this.jogada === 9) {
-      jogadaText.setText('Velha');
+      this.add.text(890, 350, 'Velha', { fontSize: '35px', color: '#fff', fontFamily: 'monospace' });
     }
+  }
+
+  update() {
     if (this.resultado > 0 || this.jogada === 9) {
+      
+      this.tabuleiro
       this.scene.launch('Restart');
       this.jogada = 0;
       this.resultado = 0;
